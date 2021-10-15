9d24211b12e56365e610e6c3b1b0c3631991dbe8
Hyeseong-int <a01025819869@gmail.com>
2021-10-15 14:00:54 +0900

    access control_update&delete auth error correction

--- routes/topic.js
+++ routes/topic.js
@@ -7,6 +7,7 @@ var template = require('../lib/template.js');
 var auth = require('../lib/auth');
 var db = require('../lib/db');
 const shortid = require('shortid');
+const { resolveNaptr } = require('dns');
 
 router.get('/create', function (request, response) {
   if (!auth.isOwner(request, response)) {
@@ -54,6 +55,11 @@ router.get('/update/:pageId', function (request, response) {
     return false;
   }
   var topic = db.get('topics').find({id:request.params.pageId}).value();
+  if(topic.user_id !== request.user){
+    request.flash('error', 'Not yours!!');
+    console.log(`ERROR! NOT YOURS!\n writer: ${topic.user_id} \n login: ${request.user}`);
+    return response.redirect('/');
+  }
   var title = topic.title;
   var description = topic.description;
   var list = template.list(request.list);
@@ -85,11 +91,20 @@ router.post('/update_process', function (request, response) {
   var id = post.id;
   var title = post.title;
   var description = post.description;
-  fs.rename(`data/${id}`, `data/${title}`, function (error) {
-    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-      response.redirect(`/topic/${title}`);
-    })
-  });
+  
+  var topic = db.get('topics').find({id:id}).value();
+  console.log("TOPIC:", topic);
+  if(topic.user_id !== request.user){
+    request.flash('error', 'Not yours!!');
+    console.log(`ERROR! NOT YOURS!\n writer: ${topic.user_id} \n login: ${request.user}`);
+    return response.redirect('/');
+  }
+  db.get('topics').find({id:id}).assign({
+    title:title,
+    description:description
+  }).write();
+
+  response.redirect(`/topic/${topic.id}`);
 });
 
 router.post('/delete_process', function (request, response) {
@@ -99,10 +114,13 @@ router.post('/delete_process', function (request, response) {
   }
   var post = request.body;
   var id = post.id;
-  var filteredId = path.parse(id).base;
-  fs.unlink(`data/${filteredId}`, function (error) {
-    response.redirect('/');
-  });
+  var topic = db.get('topics').find({id:id}).value();
+  if(topic.user_id !== request.user){
+    request.flash('error', 'Not yours!!!');
+    return response.redirect('/');
+  }
+  db.get('topics').remove({id:id}).write();
+  response.redirect('/');
 });
 
 router.get('/:pageId', function (request, response, next) {
@@ -127,7 +145,7 @@ router.get('/:pageId', function (request, response, next) {
     ` <a href="/topic/create">create</a>
         <a href="/topic/update/${topic.id}">update</a>
         <form action="/topic/delete_process" method="post">
-          <input type="hidden" name="id" value="${sanitizedTitle}">
+          <input type="hidden" name="id" value="${topic.id}">
           <input type="submit" value="delete">
         </form>`,
     auth.statusUI(request, response)
